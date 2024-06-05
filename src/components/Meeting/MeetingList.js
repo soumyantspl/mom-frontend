64b9da976fc4b4b223489e097193e110451a1b5c
soumyantspl <soumya.mishra@ntspl.co.in>
2024-06-05 13:17:14 +0530

    list meeting action details

--- src/components/Meeting/MeetingList.js
+++ src/components/Meeting/MeetingList.js
@@ -14,6 +14,7 @@ import { fetchMeetingList } from "../../redux/actions/meetingActions.js/listMeet
 import LoaderButton from "../Common/LoaderButton";
 import Loader from "../Common/Loader";
 import Alert from "../Common/Alert";
+import MeetingDropDown from "./MeetingDropDown";
 
 const MeetingList = () => {
   const userData = JSON.parse(localStorage.getItem("userData"));
@@ -284,118 +285,12 @@ const MeetingList = () => {
                         </span>
                         {/* <Button variant="success"  size="sm" style={{fontWeight:"bold"}} >Scheduled</Button> */}
                       </td>
-                      <td data-label="Action Due">0/0</td>
+                      <td data-label="Action Due">{meeting.actionDetail.filter((item)=>item.isComplete).length}/{meeting.actionDetail.length}</td>
                       <td data-label="Action">
                         <div className="d-inline-block menu-dropdown custom-dropdown">
                           <Dropdown>
-                            <Dropdown.Toggle>
-                              <svg
-                                xmlns="http://www.w3.org/2000/svg"
-                                width="16"
-                                height="16"
-                                fill="#000"
-                                className="bi bi-three-dots-vertical"
-                                viewBox="0 0 16 16"
-                              >
-                                <path d="M9.5 13a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0m0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0m0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0" />
-                              </svg>
-                            </Dropdown.Toggle>
 
-                            <Dropdown.Menu>
-                              <Dropdown.Item href="#/action-2">
-                                <Link
-                                  to="/viewMeetingDetails"
-                                  style={{
-                                    textDecoration: "none",
-                                    color: "black",
-                                  }}
-                                >
-                                  <svg
-                                    xmlns="http://www.w3.org/2000/svg"
-                                    width="17"
-                                    height="17"
-                                    fill="currentColor"
-                                    className="bi bi-eye me-2"
-                                    viewBox="0 0 16 16"
-                                  >
-                                    <path d="M16 8s-3-5.5-8-5.5S0 8 0 8s3 5.5 8 5.5S16 8 16 8M1.173 8a13 13 0 0 1 1.66-2.043C4.12 4.668 5.88 3.5 8 3.5s3.879 1.168 5.168 2.457A13 13 0 0 1 14.828 8q-.086.13-.195.288c-.335.48-.83 1.12-1.465 1.755C11.879 11.332 10.119 12.5 8 12.5s-3.879-1.168-5.168-2.457A13 13 0 0 1 1.172 8z" />
-                                    <path d="M8 5.5a2.5 2.5 0 1 0 0 5 2.5 2.5 0 0 0 0-5M4.5 8a3.5 3.5 0 1 1 7 0 3.5 3.5 0 0 1-7 0" />
-                                  </svg>
-                                  View Meeting Details
-                                </Link>
-                              </Dropdown.Item>
-                              <Dropdown.Item href="#/action-1">
-                                <Link
-                                  to="/meeting/write-minutes"
-                                  style={{
-                                    textDecoration: "none",
-                                    color: "black",
-                                  }}
-                                >
-                                  <svg
-                                    xmlns="http://www.w3.org/2000/svg "
-                                    width="16"
-                                    height="16"
-                                    fill="currentColor"
-                                    className="bi bi-pencil-square me-2"
-                                    viewBox="0 0 16 16"
-                                  >
-                                    <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z" />
-                                    <path
-                                      fill-rule="evenodd"
-                                      d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5z"
-                                    />
-                                  </svg>
-                                  Write Minutes
-                                </Link>
-                              </Dropdown.Item>
-                              <Dropdown.Item href="#/action-3">
-                                <Link
-                                  style={{
-                                    textDecoration: "none",
-                                    color: "black",
-                                  }}
-                                >
-                                  <svg
-                                    xmlns="http://www.w3.org/2000/svg"
-                                    width="16"
-                                    height="16"
-                                    fill="currentColor"
-                                    className="bi bi-pencil-square me-2"
-                                    viewBox="0 0 16 16"
-                                  >
-                                    <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z" />
-                                    <path
-                                      fill-rule="evenodd"
-                                      d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5z"
-                                    />
-                                  </svg>
-                                  Edit Meeting
-                                </Link>
-                              </Dropdown.Item>
-                              <Dropdown.Divider />
-                              <Dropdown.Item eventKey="4">
-                                <Link
-                                  style={{
-                                    textDecoration: "none",
-                                    color: "black",
-                                  }}
-                                >
-                                  <svg
-                                    xmlns="http://www.w3.org/2000/svg"
-                                    width="17"
-                                    height="17"
-                                    fill="currentColor"
-                                    className="bi bi-x-circle me-2"
-                                    viewBox="0 0 16 16"
-                                  >
-                                    <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16" />
-                                    <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708" />
-                                  </svg>
-                                  Cancel Meeting
-                                </Link>
-                              </Dropdown.Item>
-                            </Dropdown.Menu>
+                          <MeetingDropDown status={meeting.meetingStatus.status}/>
                           </Dropdown>
 
                           {/* {buttonStatus ? <VariantsExample /> : null} */}
