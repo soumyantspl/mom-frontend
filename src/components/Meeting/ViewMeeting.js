8a5df52c1d73d54b8d95956402b549d6573f72dc
soumyantspl <soumya.mishra@ntspl.co.in>
2024-06-13 18:40:26 +0530

    create meeting step 1 step count

--- src/components/Meeting/ViewMeeting.js
+++ src/components/Meeting/ViewMeeting.js
@@ -27,10 +27,10 @@ const ViewMeeting = () => {
 
         <div className="form-group mb-2">
           <div className="row">
-            <div className="col-md-4">
+            <div className="col-xl-4 col-lg-4 col-md-4 col-sm-12 col-12">
               <label className="mb-1">Meeting Mode</label>
             </div>
-            <div className="col-md-4">
+            <div className="col-xl-4 col-lg-4 col-md-4 col-sm-12 col-12">
               <p>Physical Meeting</p>
             </div>
           </div>
@@ -38,10 +38,10 @@ const ViewMeeting = () => {
 
         <div className="form-group mb-2">
           <div className="row">
-            <div className="col-md-4">
+            <div className="col-xl-4 col-lg-4 col-md-4 col-sm-12 col-12">
               <label className="mb-1">Location</label>
             </div>
-            <div className="col-md-8">
+            <div className="col-xl-8 col-lg-8 col-md-8 col-sm-12 col-12">
               <p>NTSPL, 5th Floor, Conference room</p>
             </div>
           </div>
@@ -49,10 +49,10 @@ const ViewMeeting = () => {
 
         <div className="form-group mb-2">
           <div className="row">
-            <div className="col-md-4">
+            <div className="col-xl-4 col-lg-4 col-md-4 col-sm-12 col-12">
               <label className="mb-1">Meeting Link</label>
             </div>
-            <div className="col-md-8">
+            <div className="col-xl-8 col-lg-8 col-md-8 col-sm-12 col-12">
               <p>https://meet.google.com/hkv-hxsf-whf</p>
             </div>
           </div>
@@ -60,10 +60,10 @@ const ViewMeeting = () => {
 
         <div className="form-group mb-2">
           <div className="row">
-            <div className="col-md-4">
+            <div className="col-xl-4 col-lg-4 col-md-4 col-sm-12 col-12">
               <label className="mb-1">Date & Time</label>
             </div>
-            <div className="col-md-8">
+            <div className="col-xl-8 col-lg-8 col-md-8 col-sm-12 col-12">
               <p>03 Jan 2024 , 5:40 PM to 7:00 PM</p>
             </div>
           </div>
@@ -71,10 +71,10 @@ const ViewMeeting = () => {
 
         <div className="form-group mb-2">
           <div className="row align-items-center">
-            <div className="col-md-4">
+            <div className="col-xl-4 col-lg-4 col-md-4 col-sm-12 col-12">
               <label className="pb-1"> Attendee(s) </label>
             </div>
-            <div className="col-md-8">
+            <div className="col-xl-8 col-lg-8 col-md-8 col-sm-12 col-12">
               <div className="attendees">
                 <div className="attendee-list">PK</div>
                 <div className="attendee-list">SL</div>
@@ -104,10 +104,10 @@ const ViewMeeting = () => {
             <div className="p-3">
               <div className="pb-3 form-group">
                 <div className="row">
-                  <div className="col-md-4">
+                  <div className="col-xl-4 col-lg-4 col-md-4 col-sm-12 col-12">
                     <label className="mt-1 mb-1">Agenda Title</label>
                   </div>
-                  <div className="col-md-8">
+                  <div className="col-xl-8 col-lg-8 col-md-8 col-sm-12 col-12">
                     <p> Agenda Item 1</p>
                   </div>
                 </div>
@@ -115,10 +115,10 @@ const ViewMeeting = () => {
 
               <div className=" pb-3 form-group">
                 <div className="row">
-                  <div className="col-md-4">
+                  <div className="col-xl-4 col-lg-4 col-md-4 col-sm-12 col-12">
                     <label className="mt-2 topic">Topic to Discuss</label>
                   </div>
-                  <div className="col-md-8">
+                  <div className="col-xl-8 col-lg-8 col-md-8 col-sm-12 col-12">
                     <p className="mb-2">
                       Lorem, ipsum dolor sit amet consectetur adipisicing elit.
                       Modi molestiae architecto tempora quibusdam magni illum
@@ -130,10 +130,10 @@ const ViewMeeting = () => {
 
               <div className=" form-group">
                 <div className="row">
-                  <div className="col-md-4">
+                  <div className="col-xl-4 col-lg-4 col-md-4 col-sm-12 col-12">
                     <label className="mt-1 mb-1">Timeline</label>
                   </div>
-                  <div className="col-md-8">
+                  <div className="col-xl-8 col-lg-8 col-md-8 col-sm-12 col-12">
                     <p> 30 Min</p>
                   </div>
                 </div>
@@ -153,10 +153,10 @@ const ViewMeeting = () => {
             <div className="p-3">
               <div className="pb-3 form-group">
                 <div className="row">
-                  <div className="col-md-4">
+                  <div className="col-xl-4 col-lg-4 col-md-4 col-sm-12 col-12">
                     <label className="mt-1 mb-1">Agenda Title</label>
                   </div>
-                  <div className="col-md-8">
+                  <div className="col-xl-8 col-lg-8 col-md-8 col-sm-12 col-12">
                     <p> Agenda Item 2</p>
                   </div>
                 </div>
@@ -164,10 +164,10 @@ const ViewMeeting = () => {
 
               <div className=" pb-3 form-group">
                 <div className="row">
-                  <div className="col-md-4">
+                  <div className="col-xl-4 col-lg-4 col-md-4 col-sm-12 col-12">
                     <label className="mt-2 topic">Topic to Discuss</label>
                   </div>
-                  <div className="col-md-8">
+                  <div className="col-xl-8 col-lg-8 col-md-8 col-sm-12 col-12">
                     <p className="mb-2">
                       Lorem, ipsum dolor sit amet consectetur adipisicing elit.
                       Modi molestiae architecto tempora quibusdam magni illum
@@ -179,10 +179,10 @@ const ViewMeeting = () => {
 
               <div className=" form-group">
                 <div className="row">
-                  <div className="col-md-4">
+                  <div className="col-xl-4 col-lg-4 col-md-4 col-sm-12 col-12">
                     <label className="mt-1 mb-1">Timeline</label>
                   </div>
-                  <div className="col-md-8">
+                  <div className="col-xl-8 col-lg-8 col-md-8 col-sm-12 col-12">
                     <p> 30 Min</p>
                   </div>
                 </div>
